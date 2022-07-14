06d48c74c105d0c609265982ce4fed41f0f73cb5
Roro1005 <105412858+Roro1005@users.noreply.github.com>
2022-07-14 15:59:46 +0900

    Update Functions.cs

--- PasswordGenerator/PasswordGenerator/Functions.cs
+++ PasswordGenerator/PasswordGenerator/Functions.cs
@@ -8,13 +8,39 @@ namespace PasswordGenerator
 {
     internal class Functions
     {
+
+        public static (int,bool) ParseArgs(string[] args)
+        {
+            //桁数の特定
+            if (args.Length < 1) return (10,true);
+
+            int digits;
+            if(!int.TryParse(args[0],out digits))
+            {
+                digits= 10;
+            }
+
+            //第二引数が存在して、"0"だったら記号は使わない
+            if (args.Length < 2) return (digits, true);
+            bool useMark = args[1] != "0";
+
+            return (digits, useMark);
+        }
+
         public static char GetRandomletter(char[] big,char[] little,char[]number,
-                                           System.Random rand)
+                                           System.Random rand, bool useMark)
         {
             char[] target;
             //まずはどの配列から文字を選ぶかサイコロを振る
-            switch(rand.Next(3))
+            int randMax;
+            if (useMark)
+                randMax = 4;
+            else randMax = 3;
+            switch(rand.Next(randMax))
             {
+                case 3:
+                    target = useMark;
+                    break;
                 case 0:
                     target = big;
                     break;
