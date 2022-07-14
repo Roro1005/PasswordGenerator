47c0225202eb8d25a936eb1ce5dc655ee1f60e3d
Roro1005 <105412858+Roro1005@users.noreply.github.com>
2022-07-14 15:21:25 +0900

    passwordgenerator

---
+++ PasswordGenerator/PasswordGenerator/Functions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    internal class Functions
+    {
+        public static char GetRandomletter(char[] big,char[] little,char[]number,
+                                           System.Random rand)
+        {
+            char[] target;
+            //まずはどの配列から文字を選ぶかサイコロを振る
+            switch(rand.Next(3))
+            {
+                case 0:
+                    target = big;
+                    break;
+                case 1:
+                    target = little;
+                    break;
+                default:
+                    target = number;
+                    break;
+            }
+
+            //次にtargetの中からランダムな文字列を選ぶ
+            return target[rand.Next(target.Length)];
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using PasswordGenerator;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var rand = new System.Random();
+
+            char[] bigLetters = new char[]
+            {
+                'A','B','C','D','E','F','G','H','I','J','K',
+                'L','M','N','O','P','Q','R','S','T','U','V',
+                'W','X','Y','Z'
+            };
+            char[] littleLetters = new char[]
+            {
+                'a','b','c','d','e','f','g','h','i','j','k',
+                'l','m','n','o','p','q','r','s','t','u','v',
+                'w','x','y','z'
+            };
+            char[] numberLetters = new char[]
+            {
+                '0','1','2','3','4','5','6','7','8','9'
+            };
+
+            string result = "";
+            for(int i =0;i< 10;i++)
+            {
+                char letter=Functions.GetRandomletter
+                    (bigLetters, littleLetters,numberLetters,rand);
+
+                result += letter;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
